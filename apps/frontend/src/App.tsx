0b195e47578fb17db56faab0668612312a9db2fd
Anthony Powell <anthonypowell@hey.com>
2022-10-07 19:23:10 -0400

    format

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,12 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <h1 className="title">
-        ChattreQL
-      </h1>
+      <h1 className="title">ChattreQL</h1>
       <p className="description">
-        Built With{" "}
-        <a href="https://turborepo.org/">Turborepo</a> +{" "}
+        Built With <a href="https://turborepo.org/">Turborepo</a> +{" "}
         <a href="https://vitejs.dev/">Vite</a>
       </p>
     </div>
