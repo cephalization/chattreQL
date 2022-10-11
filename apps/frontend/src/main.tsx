11d9c8b7272c97be1d9f540cab2a8f6224b3884e
Anthony Powell <anthonypowell@hey.com>
2022-10-11 18:52:58 -0400

    Connect frontend to Apollo Server via Apollo Client

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,11 +2,23 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const URL = import.meta.env.VITE_API_URI;
+
+const client = new ApolloClient({
+  uri: URL,
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
