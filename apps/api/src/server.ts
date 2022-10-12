7536b745ff1421854259cea6c98081d6dca9bfc1
Anthony Powell <anthonypowell@hey.com>
2022-10-12 11:45:25 -0400

    Add websocket apollo listener for subscription support

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,28 +1,57 @@
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import http from "http";
+import http, { createServer as httpCreateServer } from "http";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
 
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./typeDefs";
 
+const GRAPH_QL_PATH = "/";
+
 export const createServer = async () => {
+  // requirements for the express version of the server
   const app = express();
   const httpServer = http.createServer(app);
+  // requirements websocket version of the server
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
 
+  // instantiate the websocket apollo server
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: GRAPH_QL_PATH,
+  });
+  const wsServerCleanup = useServer({ schema }, wsServer);
+
+  // instantiate the express apollo server
   const server = new ApolloServer({
-    typeDefs,
+    schema,
     csrfPrevention: true,
     cache: "bounded",
     resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: [
+      // properly shutdown the http express server when api terminates
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      // properly shutdown the ws server when api terminates
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await wsServerCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
   });
 
   await server.start();
 
   server.applyMiddleware({
     app,
-    path: "/",
+    path: GRAPH_QL_PATH,
   });
 
   return {
